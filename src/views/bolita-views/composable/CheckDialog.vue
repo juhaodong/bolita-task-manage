220e01344f0e3b44737e720d8f14983ab3e004a5
Haodong Ju <juhaodong@gmail.com>
2023-08-13 10:02:29 +0200

    完成了

--- src/views/bolita-views/composable/CheckDialog.vue
+++ src/views/bolita-views/composable/CheckDialog.vue
@@ -10,22 +10,27 @@
 </script>
 
 <template>
-  <n-modal :show="checkDialog.show" @close="checkDialog.close()">
-    <n-card :title="checkDialog.title" style="max-width: 700px">
-      <normal-form
-        :form-fields="field"
-        @cancel="checkDialog.cancel"
-        @submit="checkDialog.confirm"
-        :show-buttons="false"
-      >
-        <template #extraSubmitButton="{ submit }">
-          <n-button @click="submit" type="success">通过审核</n-button>
-        </template>
-        <template #extraCancelButton="{ cancel }">
-          <n-button @click="cancel" type="error">拒绝</n-button>
-        </template>
-      </normal-form>
-    </n-card>
+  <n-modal
+    :preset="'card'"
+    style="max-width: 700px"
+    :title="checkDialog.title"
+    :show="checkDialog.show"
+    @close="checkDialog.close()"
+    closable
+  >
+    <normal-form
+      :form-fields="field"
+      @cancel="checkDialog.cancel"
+      @submit="checkDialog.confirm"
+      :show-buttons="false"
+    >
+      <template #extraSubmitButton="{ submit }">
+        <n-button @click="submit" type="success">通过审核</n-button>
+      </template>
+      <template #extraCancelButton="{ cancel }">
+        <n-button @click="cancel" type="error">拒绝</n-button>
+      </template>
+    </normal-form>
   </n-modal>
 </template>
 
